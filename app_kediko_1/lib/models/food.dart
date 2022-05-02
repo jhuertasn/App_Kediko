e26ccf4515c3c20f193fd44e36f42aae95fd8e98
jhuertasn <81602342+jhuertasn@users.noreply.github.com>
2022-05-02 17:13:37 -0500

    creacion de food.dart

--- app_kediko_1/lib/models/food.dart
+++ app_kediko_1/lib/models/food.dart
@@ -9,10 +9,114 @@ class Food {
   num quantity;
   List<Map<String, String>> ingredients;
   String about;
-  bool highlight;
+  bool highLight;
   Food(this.imgUrl, this.desc, this.name, this.waitTime, this.score, this.cal,
       this.price, this.quantity, this.ingredients, this.about,
-      {this.highlight = false});
-  //static List<Food> generateRecomendFoods(){
+      {this.highLight = false});
 
+  static List<Food> generateRecomendFoods() {
+    return [
+      Food(
+          'assets/images/dish1.png',
+          'No1. in Sales',
+          'Soba Soup',
+          '50 min',
+          4.8,
+          '325 kcal',
+          12,
+          1,
+          [
+            {
+              'Noodle': 'assets/images/ingre1.png',
+            },
+            {
+              'Shrimp': 'assets/images/ingre2.png',
+            },
+            {
+              'Egg': 'assets/images/ingre3.png',
+            },
+            {
+              'Scallion': 'assets/images/ingre4.png',
+            },
+          ],
+          'Simples pasos, este ramen es una sopa japonesa'),
+      Food(
+          'assets/images/dish2.png',
+          'No1. in Sales',
+          'Soba Soup',
+          '50 min',
+          4.8,
+          '325 kcal',
+          12,
+          1,
+          [
+            {
+              'Noodle': 'assets/images/ingre1.png',
+            },
+            {
+              'Shrimp': 'assets/images/ingre2.png',
+            },
+            {
+              'Egg': 'assets/images/ingre3.png',
+            },
+            {
+              'Scallion': 'assets/images/ingre4.png',
+            },
+          ],
+          'Simples pasos, este ramen es una sopa japonesa'),
+    ];
+  }
+
+  static List<Food> generatePopularFoods() {
+    return [
+      Food(
+          'assets/images/dish4.png',
+          'Most Popular',
+          'Tomato Checken',
+          '50 min',
+          4.8,
+          '325 kcal',
+          14,
+          1,
+          [
+            {
+              'Noodle': 'assets/images/ingre1.png',
+            },
+            {
+              'Shrimp': 'assets/images/ingre2.png',
+            },
+            {
+              'Egg': 'assets/images/ingre3.png',
+            },
+            {
+              'Scallion': 'assets/images/ingre4.png',
+            },
+          ],
+          'Simples pasos, este ramen es una sopa japonesa'),
+      Food(
+          'assets/images/dish2.png',
+          'Low Fat',
+          'Sai Ua Samun Phrai',
+          '50 min',
+          4.8,
+          '325 kcal',
+          12,
+          0,
+          [
+            {
+              'Noodle': 'assets/images/ingre1.png',
+            },
+            {
+              'Shrimp': 'assets/images/ingre2.png',
+            },
+            {
+              'Egg': 'assets/images/ingre3.png',
+            },
+            {
+              'Scallion': 'assets/images/ingre4.png',
+            },
+          ],
+          'Simples pasos, para un Sai Ua Samun Phrai'),
+    ];
+  }
 }
