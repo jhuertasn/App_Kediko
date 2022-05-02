a85018aa5dd15b4c071132bcb3bcbea30ef76140
jhuertasn <81602342+jhuertasn@users.noreply.github.com>
2022-05-02 18:18:34 -0500

    agregado comandos y vistas en home

--- app_kediko_1/lib/screens/home/home.dart
+++ app_kediko_1/lib/screens/home/home.dart
@@ -1,4 +1,9 @@
 import 'package:app_kediko_1/constants/colors.dart';
+//importando los colores de la app
+import 'package:app_kediko_1/models/restaurant.dart';
+//importando la libreria Restaurant
+import 'package:app_kediko_1/screens/home/widget/food_list.dart';
+//importando foofList
 import 'package:app_kediko_1/screens/home/widget/restaurant_info.dart';
 //importando los colores de la app
 import 'package:flutter/material.dart';
@@ -13,7 +18,7 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   var selected = 0;
   //declarar la variable del selector
-
+  final restaurant = Restaurant.generateRestaurant();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,6 +31,11 @@ class _HomePageState extends State<HomePage> {
             Icons.search_outlined,
           ),
           RestaurantInfo(),
+          FoodList(selected, (int index) {
+            setState(() {
+              selected = index;
+            });
+          }, restaurant)
         ],
       ),
     );
