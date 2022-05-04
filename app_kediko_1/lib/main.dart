afa7e914aa21383e824b6e96b842791aae157ebb
jhuertasn <81602342+jhuertasn@users.noreply.github.com>
2022-05-04 18:42:16 -0500

    Update main.dart

--- app_kediko_1/lib/main.dart
+++ app_kediko_1/lib/main.dart
@@ -1,15 +1,21 @@
+import 'package:flutter/services.dart';
+//llamando a los servicios de chorme
+import 'package:flutter/material.dart';
 import 'package:app_kediko_1/screens/home/home.dart';
+//llamando a home.dart
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+    ));
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      debugShowCheckedModeBanner: false,
+      //ocultar la barra de estado
+      title: 'Kediko App',
       home: HomePage(),
     );
   }
